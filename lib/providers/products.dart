b4e7d3697edea0b2ec433e46ac4226066848f1f5
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-09-25 13:02:31 +0100

    Section 8 : 204

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -37,9 +37,17 @@ class Products with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
+  // var _showFavoritesOnly = false;
 
   List<Product> get items {
+    // if(_showFavoritesOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
